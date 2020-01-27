337c6f882eb40cbf64a1c545e6f7173871770de2
Michael Mendoza <askmichaelmendoza@gmail.com>
2020-01-27 15:27:16 +0000

    Updated and fixed resnet

--- examples/cifar/cifar5.py
+++ examples/cifar/cifar5.py
@@ -16,64 +16,61 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten
-from tensorflow.keras.layers import Conv2D, MaxPooling2D
- 
-# Import Dataset
-from data_loader import DataLoader
-cifar = DataLoader()
+import numpy as np
 
 # Training Parameters
-batch_size = 32
-epochs = 10
+epochs = 30
 
 # Network Parameters
-_WIDTH = 32; _HEIGHT = 32; _CHANNELS = 3 
-NUM_INPUTS = _WIDTH * _HEIGHT * _CHANNELS 
-NUM_OUTPUTS = 10
+WIDTH = 32; HEIGHT = 32; CHANNELS = 3 
 
-# Resnet 
-def res_net_block(input_data, filters):
-    x = layers.Conv2D(filters, 3, padding="same", activation="relu")(input_data)
-    x = layers.BatchNormalization()(x)
-    x = layers.Conv2D(filters, 3, padding="same", activation=None)(x)
-    x = layers.layers.BatchNormalization()(x)
-    x = layers.Add()([x, input_data])
-    x = layers.Activation('relu')(x)
-    return x
+# Import Dataset
+(x_train, y_train), (x_test, y_test) = tf.keras.datasets.cifar10.load_data()
 
-inputs = keras.Input(shape = (_HEIGHT, _WIDTH, _CHANNELS))
-x = layers.Conv2D(32, 3, padding="same", activation="relu")(inputs)
-x = layers.Conv2D(64, 3, padding="same", activation="relu")(x)
-x = layers.MaxPooling2D(3)(x) # Why 3?
+train_dataset = tf.data.Dataset.from_tensor_slices((x_train, y_train)).batch(64).shuffle(10000)
+train_dataset = train_dataset.map(lambda x, y: (tf.cast(x, tf.float32) / 255.0, y))
+train_dataset = train_dataset.map(lambda x, y: (tf.image.random_flip_left_right(x), y))
+train_dataset = train_dataset.repeat()
 
-res_block_count = 10
-for _ in range(res_block_count):
-    x = res_net_block(x, 64)
+valid_dataset = tf.data.Dataset.from_tensor_slices((x_test, y_test)).batch(5000).shuffle(10000)
+valid_dataset = valid_dataset.map(lambda x, y: (tf.cast(x, tf.float32) / 255.0, y))
+valid_dataset = valid_dataset.repeat()
 
-x = layers.Conv2D(64, 3, activation="relu")(x)
-x = layers.GlobalAveragePooling2D()(x)
-x = layers.Dense(256, activation='relu')x()
-x = layers.Dropout(0.5)(x)
-outputs = layers.Dense(NUM_OUTPUTS, activation='softmax')(x)
+def resnet():
 
-model = keras.Model(inputs, outputs)
+    def res_net_block(input_data, filters, conv_size):
+      x = layers.Conv2D(filters, conv_size, activation='relu', padding='same')(input_data)
+      x = layers.BatchNormalization()(x)
+      x = layers.Conv2D(filters, conv_size, activation=None, padding='same')(x)
+      x = layers.BatchNormalization()(x)
+      x = layers.Add()([x, input_data])
+      x = layers.Activation('relu')(x)
+      return x
 
-# Define Loss and Optimizier
-model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['categorical_accuracy'])
-model.summary()
+    inputs = keras.Input(shape=(HEIGHT, WIDTH, CHANNELS))
+    x = layers.Conv2D(32, 3, activation='relu')(inputs)
+    x = layers.Conv2D(64, 3, activation='relu')(x)
+    x = layers.MaxPooling2D(3)(x)
 
-# Reshape data
-cifar.x_train = cifar.x_train.reshape(-1, _WIDTH, _HEIGHT, _CHANNELS)
-cifar.x_test = cifar.x_test.reshape(-1, _WIDTH, _HEIGHT, _CHANNELS)
+    num_res_net_blocks = 10
+    for i in range(num_res_net_blocks):
+        x = res_net_block(x, 64, 3)
 
-# Train the model, iterating on the data in batches of 32 samples
-model.fit(cifar.x_train, cifar.y_train, epochs=epochs, batch_size=batch_size)
+    x = layers.Conv2D(64, 3, activation='relu')(x)
+    x = layers.GlobalAveragePooling2D()(x)
+    x = layers.Dense(256, activation='relu')(x)
+    x = layers.Dropout(0.5)(x)
+    outputs = layers.Dense(10, activation='softmax')(x)
 
-# Evaluate
-print('')
-print('Evaluate:')
-loss_and_metrics = model.evaluate(cifar.x_test, cifar.y_test, verbose=1)
-print('')
-print('Summary: Loss over the test dataset: %.2f, Accuracy: %.2f' % (loss_and_metrics[0], loss_and_metrics[1]))
+    model = keras.Model(inputs, outputs)
+    return model
+
+model = resnet()
+model.compile(optimizer=keras.optimizers.Adam(),
+              loss='sparse_categorical_crossentropy',
+              metrics=['acc'])
+
+# Train and Evaluate model
+model.fit(train_dataset, epochs=epochs, steps_per_epoch=200,
+          validation_data=valid_dataset,
+          validation_steps=3)
